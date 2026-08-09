4245dba8e2d9cbcf8d0a2af49ad34e315a5049b6
l <96929613+AdrenalineGremlin@users.noreply.github.com>
2026-08-09 17:50:21 -0400

    first commit

---
+++ src/main/java/com/dtnmessaging/dtn_messaging/DtnMessagingApplication.java
@@ -0,0 +1,13 @@
+package com.dtnmessaging.dtn_messaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtnMessagingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DtnMessagingApplication.class, args);
+	}
+
+}
