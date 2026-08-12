47893080c75d8e44be129f16bef79d5cea2ba448
l <96929613+AdrenalineGremlin@users.noreply.github.com>
2026-08-12 17:05:39 -0400

    node entity finished

--- src/main/java/com/dtnmessaging/dtn_messaging/model/Node.java
+++ src/main/java/com/dtnmessaging/dtn_messaging/model/Node.java
@@ -1,5 +1,25 @@
 package com.dtnmessaging.dtn_messaging.model;
 
+import java.util.UUID;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Node {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+    private String name;
+    private boolean connectivity;
 
 }
