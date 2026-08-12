216c5a58910df585c7a12cf6165675487d136e9f
l <96929613+AdrenalineGremlin@users.noreply.github.com>
2026-08-12 19:53:11 -0400

    message entity finished

--- src/main/java/com/dtnmessaging/dtn_messaging/model/Message.java
+++ src/main/java/com/dtnmessaging/dtn_messaging/model/Message.java
@@ -1,5 +1,32 @@
 package com.dtnmessaging.dtn_messaging.model;
 
+import java.time.Instant;
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Message {
+    private @Id @GeneratedValue UUID id;
+    private UUID logicalID;
+    private UUID nodeID;
+    private String content;
+    @Enumerated(EnumType.STRING)
+    private MessageStatus status;
+    @Enumerated(EnumType.STRING)
+    private Priority priority;
+    private Instant lastModified;
 
 }
