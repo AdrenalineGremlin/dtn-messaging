b9541c99751c9db1596400db70b00285d1838190
l <96929613+AdrenalineGremlin@users.noreply.github.com>
2026-08-16 14:15:32 -0400

    logicalid check

--- src/main/java/com/dtnmessaging/dtn_messaging/service/SyncService.java
+++ src/main/java/com/dtnmessaging/dtn_messaging/service/SyncService.java
@@ -25,7 +25,18 @@ public class SyncService {
         }
         // fetch pending messages for current node
         List<Message> messages = messageRepository.findByNodeIdAndStatus(nodeID, MessageStatus.PENDING);
-        ;
+
+        for (Message m : messages) {
+            //check if ther are any messages sharing the same logicalId
+            //store the ids in a list
+            List<Message> twin = messageRepository.findByLogicalID(m.getLogicalID());
+            if (twin.size() == 1) {
+                //if none share the id status is synced
+                m.setStatus(MessageStatus.SYNCED);
+                messageRepository.save(m);
+            }.
+
+        }
     }
 
     public SyncService(MessageRepository messageRepository, NodeRepository nodeRepository) {
