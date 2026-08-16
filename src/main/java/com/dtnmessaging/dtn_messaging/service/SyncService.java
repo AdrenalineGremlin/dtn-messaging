b60af74158bd272cdddd9641c78e63dfd84f4c10
l <96929613+AdrenalineGremlin@users.noreply.github.com>
2026-08-16 11:05:34 -0400

    started sync node method

--- src/main/java/com/dtnmessaging/dtn_messaging/service/SyncService.java
+++ src/main/java/com/dtnmessaging/dtn_messaging/service/SyncService.java
@@ -1,7 +1,13 @@
 package com.dtnmessaging.dtn_messaging.service;
 
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.stereotype.Service;
 
+import com.dtnmessaging.dtn_messaging.model.Message;
+import com.dtnmessaging.dtn_messaging.model.MessageStatus;
+import com.dtnmessaging.dtn_messaging.model.Node;
 import com.dtnmessaging.dtn_messaging.repository.MessageRepository;
 import com.dtnmessaging.dtn_messaging.repository.NodeRepository;
 
@@ -9,6 +15,18 @@ import com.dtnmessaging.dtn_messaging.repository.NodeRepository;
 public class SyncService {
     private final MessageRepository messageRepository;
     private final NodeRepository nodeRepository;
+
+    public void syncNode(UUID nodeID) {
+        // get the node and check if theres connectivity
+        Node nr = nodeRepository.findById(nodeID).orElseThrow();
+        if (!nr.isConnectivity()) {
+            // if false there is nothing to sync - return
+            return;
+        }
+        // fetch pending messages for current node
+        List<Message> messages = messageRepository.findByNodeIdAndStatus(nodeID, MessageStatus.PENDING);
+        ;
+    }
 
     public SyncService(MessageRepository messageRepository, NodeRepository nodeRepository) {
         this.messageRepository = messageRepository;
