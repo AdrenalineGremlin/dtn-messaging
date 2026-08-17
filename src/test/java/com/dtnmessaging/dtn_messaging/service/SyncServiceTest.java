d70136a4e305e0fbafe870da32948dff94fecfeb
l <96929613+AdrenalineGremlin@users.noreply.github.com>
2026-08-17 17:02:07 -0400

    started unit testing

---
+++ src/test/java/com/dtnmessaging/dtn_messaging/service/SyncServiceTest.java
@@ -0,0 +1,17 @@
+package com.dtnmessaging.dtn_messaging.service;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import com.dtnmessaging.dtn_messaging.repository.MessageRepository;
+import com.dtnmessaging.dtn_messaging.repository.NodeRepository;
+import com.dtnmessaging.dtn_messaging.model.Message;
+import com.dtnmessaging.dtn_messaging.model.Node;
+import com.dtnmessaging.dtn_messaging.model.MessageStatus;
+import com.dtnmessaging.dtn_messaging.model.Priority;
+import java.util.List;
+import java.util.UUID;
+import java.time.Instant;
+
+public class SyncServiceTest {
+    
+}
