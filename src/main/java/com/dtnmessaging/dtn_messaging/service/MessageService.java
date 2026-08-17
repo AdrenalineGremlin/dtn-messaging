9a8bf58b161274cfefdde278bea1a0151252b889
l <96929613+AdrenalineGremlin@users.noreply.github.com>
2026-08-16 20:38:27 -0400

    finished messagesrevice

--- src/main/java/com/dtnmessaging/dtn_messaging/service/MessageService.java
+++ src/main/java/com/dtnmessaging/dtn_messaging/service/MessageService.java
@@ -1,5 +1,29 @@
 package com.dtnmessaging.dtn_messaging.service;
 
-public class MessageService {
+import java.time.Instant;
+import java.util.UUID;
 
+import org.springframework.stereotype.Service;
+
+import com.dtnmessaging.dtn_messaging.model.Message;
+import com.dtnmessaging.dtn_messaging.model.MessageStatus;
+import com.dtnmessaging.dtn_messaging.model.Priority;
+import com.dtnmessaging.dtn_messaging.repository.MessageRepository;
+
+@Service
+public class MessageService {
+    private final MessageRepository messageRepository;
+
+    public Message createMessage(String content, UUID nodeID, Priority priority) {
+        // generate fresh id with randomuuid
+        UUID freshId = UUID.randomUUID();
+        // create the message
+        Message message = new Message(null, freshId, nodeID, content, MessageStatus.PENDING, priority, Instant.now());
+        // save in repository and return
+        return messageRepository.save(message);
+    }
+
+    public MessageService(MessageRepository messageRepository) {
+        this.messageRepository = messageRepository;
+    }
 }
